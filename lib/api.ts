6f1df2cfa7964456643f8e62ff0335ded64be632
Anastasiia_Hakalo <anastasiiia.hakalo@gmail.com>
2026-06-19 12:00:11 -0600

    change api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,35 +1,39 @@
 import axios from "axios";
+import type { Note } from "@/types/note";
 
 const api = axios.create({
   baseURL: "https://notehub-public.goit.study/api",
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN ?? ""}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
   },
 });
 
-export const fetchNotes = async (query = "") => {
-  const { data } = await api.get("/notes", {
-    params: query ? { search: query } : {},
+export type NotesResponse = {
+  notes: Note[];
+  totalPages: number;
+};
+
+export const fetchNotes = async (
+  search: string = "",
+  page: number = 1
+): Promise<NotesResponse> => {
+  const { data } = await api.get<NotesResponse>("/notes", {
+    params: { search, page },
   });
 
   return data;
 };
 
-export const fetchNoteById = async (id: string) => {
-  const { data } = await api.get(`/notes/${id}`);
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const { data } = await api.get<Note>(`/notes/${id}`);
   return data;
 };
 
-export const createNote = async (body: {
-  title: string;
-  content: string;
-  tag: string;
-}) => {
-  const { data } = await api.post("/notes", body);
+export const createNote = async (body: Omit<Note, "id" | "createdAt">) => {
+  const { data } = await api.post<Note>("/notes", body);
   return data;
 };
 
 export const deleteNote = async (id: string) => {
-  const { data } = await api.delete(`/notes/${id}`);
-  return data;
+  await api.delete(`/notes/${id}`);
 };
